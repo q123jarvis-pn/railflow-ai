315e36042f958d95dd2b5e0e4dc9d008e6693cce
Aquib <q123jarvis@gmail.com>
2026-08-28 23:45:48 +0530

    Remove conflicting Vercel TypeScript entrypoint

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "../src/server/app";
-
-// Vercel Serverless Function entry point
-// Bridges Vercel serverless incoming HTTP requests directly into the Express application
-export default function handler(req: any, res: any) {
-  return app(req, res);
-}
